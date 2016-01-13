ea825e11bfd96a17ab004dcee703af35b383d2c0
Esteban Torres <me@estebantorr.es>
2016-01-12 21:00:26 -0600

    Added a way to quickly unregister from all imap notifier processes

---
+++ mutt/mutt.symfolder/imap_notifier/unregister.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo 0 | /usr/local/bin/imap_notifier -k 0
+echo 0 | /usr/local/bin/imap_notifier -k
+echo 0 | /usr/local/bin/imap_notifier -k
+echo 0 | /usr/local/bin/imap_notifier -k
+echo 0 | /usr/local/bin/imap_notifier -k
+/usr/local/bin/imap_notifier -k
+
+# vim: ft=sh
