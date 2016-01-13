82dc16efedf0434190fe5960f09f353833a1a3a8
Esteban Torres <me@estebantorr.es>
2016-01-12 21:00:09 -0600

    Fixed depth of symlinked folders

--- mutt/mutt.symfolder/imap_notifier/register.sh
+++ mutt/mutt.symfolder/imap_notifier/register.sh
@@ -6,3 +6,5 @@
 /usr/local/bin/imap_notifier -v --file ~/.mutt/imap_notifier/cocoaheads
 /usr/local/bin/imap_notifier -v --file ~/.mutt/imap_notifier/craftkollective
 /usr/local/bin/imap_notifier -v --file ~/.mutt/imap_notifier/services_ck
+
+# vim: ft=sh
