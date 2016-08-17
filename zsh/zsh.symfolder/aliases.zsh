9e3baab94141b7b84a9bee21df78a9fdd1c8f0de
Esteban Torres <me@estebantorr.es>
2016-08-16 23:24:55 -0600

    Added brew info alias

--- zsh/zsh.symfolder/aliases.zsh
+++ zsh/zsh.symfolder/aliases.zsh
@@ -20,4 +20,8 @@ eval $(thefuck --alias)
 
 alias flushcache='sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder'
 
+function brew.info {
+  grep desc $(brew --prefix)/Library/Formula/*.rb | perl -ne 'm{^.*/(.*?)\.rb.*?\"(.*)"$} and print "$1|$2\n"' | column -t -s '|' | fzf --reverse
+}
+
 # vim: ft=muttrc
